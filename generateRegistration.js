4caa8b0b5cee8670fc34c440dd929cac1220f62b
Alberto <albertoz24594@gmail.com>
2020-02-17 16:11:19 -0700

    Add to Data

--- generateRegistration.js
+++ generateRegistration.js
@@ -1,31 +1,11 @@
-var main = function()
-{
-	"use strict";
-
-	$(".inputArea .b").on("click", function(event)
-		{
-			var User = $("input.User").val();
-			var Pass = $("input.Pass").val ();
-			var $message = $("<p>");
-			$message.text( User + " " + Pass);
-			$(".outputArea1").append($message);	
-		}
-	);
-
-};
-
-$(document).ready(main);
-function clicked(idClicked){
-	var mysql = require('mysql');
-
-	var con = mysql.createConnection({
-	  host: "107.180.1.16",
-	  user: "group7-2",
-	  password: "!!Group7-2"
-	});
-
-	con.connect(function(err) {
-	  if (err) throw err;
-	  console.log("Connected!");
-	});
+function processForm(callback) {
+	let sqlStmt =  "SELECT * FROM Users"
+	//log into the database
+	var con = MySql.Execute(
+	"107.180.1.16",
+	"group7-2",
+	"!!Group7",
+	"group7-2",
+	sqlStmt
+	)
 }
